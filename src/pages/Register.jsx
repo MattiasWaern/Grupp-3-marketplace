1200b495ca4bc07f64ee88a0a1582bf4521deef9
Luna <lunahjort@hotmail.com>
2026-05-13 19:03:23 +0200

    register komponent med form state

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
 function Register() {
-
+    const [form, setForm] = useState({ username: "", email: "", password: "" });
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 }
+
+// handleChange
+
+// handleSubmit
+
+// formuläret för registeringen
 
 export default Register;
