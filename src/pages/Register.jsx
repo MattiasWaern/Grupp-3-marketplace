612edfcf4d08e949c008ad055d798633a5595f4c
Luna <lunahjort@hotmail.com>
2026-05-13 19:26:20 +0200

    tog bort state för err + loading och gjorde en enklare handleSubmit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
 
 function Register() {
-    const [form, setForm] = useState({ username: "", email: "", password: "" });
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // handleSubmit
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // trycatch
+        try {
+            const response = await fetch("http://localhost:1337/api/auth/local/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    password
+                }),
+            });
+            const data = await response.json();
+            // här kan token läggas till när inlogg finns
+            console.log(data);
+        } catch (err) {
+            console.error("Något gick fel: ", err);
+        }
+    };
+
+    // alt. handleChange?
+
+    // formuläret för registeringen
 }
-
-// handleChange
-
-// handleSubmit
-
-// formuläret för registeringen
 
 export default Register;
