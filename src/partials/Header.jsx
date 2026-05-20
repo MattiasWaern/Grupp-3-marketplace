367028ed1d38c189fc84aa518892efe3e14f9a10
Helena Rhawi <helena.rhawi@live.com>
2026-05-20 11:31:11 +0200

    Gjorde titeln till en navlink och lade till css

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,33 +1,37 @@
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
-
-
 export default function Header() {
-   const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
 
-  return <>
-    <header>
+  return (
+    <>
+      <header>
+        <div className="topRow">
+          
+          <NavLink to="/" className="titleLink">
+            <h1 className="title">The Selling Point</h1>
+          </NavLink>
 
-    <div className="topRow">
-      <h1 className="title">The Selling Point</h1>
-      <input
-        type="text"
-        placeholder="Sök produkter..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className="searchBar"
-      />
-       <nav className="navLinks">
-        <NavLink to="/new-ad">Ny annons</NavLink>
-        <NavLink to="/login">Logga in</NavLink>
-        <NavLink to="/signup" className="signUp">Skapa konto</NavLink>
-      </nav>
-  </div >
-        
-      <div className="divider"></div>
+          <input
+            type="text"
+            placeholder="Sök produkter..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="searchBar"
+          />
 
-      
+          <nav className="navLinks">
+            <NavLink to="/new-ad">Ny annons</NavLink>
+            <NavLink to="/login">Logga in</NavLink>
+            <NavLink to="/signup" className="signUp">
+              Skapa konto
+            </NavLink>
+          </nav>
+        </div>
+
+        <div className="divider"></div>
+
         <nav className="categoryLinks">
           <NavLink to="/dam">Dam</NavLink>
           <NavLink to="/herr">Herr</NavLink>
@@ -35,7 +39,7 @@ export default function Header() {
           <NavLink to="/hem">Hem</NavLink>
           <NavLink to="/elektronik">Elektronik</NavLink>
         </nav>
-       
-    </header>
-  </>;
+      </header>
+    </>
+  );
 }
