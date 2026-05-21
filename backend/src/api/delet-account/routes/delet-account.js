6ade68e28540bd6386359bfef3cc89171e2076f6
Helena Rhawi <helena.rhawi@live.com>
2026-05-21 22:57:34 +0200

    lagt till om oss sida med route

---
+++ backend/src/api/delet-account/routes/delet-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * delet-account router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::delet-account.delet-account');
---
+++ backend/src/api/delet-account/services/delet-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * delet-account service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::delet-account.delet-account');
