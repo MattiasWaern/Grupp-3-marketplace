c9e7766441f18ed5d8552c9f7e51b073a4420f7a
Luna <lunahjort@hotmail.com>
2026-05-22 13:05:33 +0200

    handleSubmit med try/catch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,4 +3,33 @@ import { useState } from 'react';
 function Login() {
     const [identifier, setIdentifier] = useState("");
     const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:1337/api/auth/local", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    identifier,
+                    password,
+                }),
+            });
+
+            const data = await response.json();
+
+            if (data.jwt) {
+                localStorage.setItem("token", data.jwt);
+                localStorage.setItem("user", JSON.stringify(data.user));
+                console.log("Inloggad!", data.user);
+            } else {
+                console.error("Inloggning misslyckades: ", data.error?.message);
+            }
+        } catch (err) {
+            console.error("Något gick fel: ", err);
+        }
+    };
 }
