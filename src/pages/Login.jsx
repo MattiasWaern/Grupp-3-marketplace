af105e0da89921f2eaa3ed8553692710e67cea4e
ReggeMeister <mattiaswaern04@gmail.com>
2026-05-25 13:01:29 +0200

    om man blir inloggad så blir man tagen till startsidan

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
     const [identifier, setIdentifier] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -26,6 +28,9 @@ function Login() {
                 localStorage.setItem("user", JSON.stringify(data.user));
                 localStorage.setItem("userId", data.user.documentId);
                 console.log("Inloggad!", data.user);
+
+                navigate("/");
+                window.location.reload();
             } else {
                 console.error("Inloggning misslyckades: ", data.error?.message);
             }
@@ -38,10 +43,19 @@ function Login() {
         <form onSubmit={handleSubmit}>
             <h2>Logga in</h2>
 
-            <input type="text" placeholder='E-mail eller användarnamn' value={identifier}
-            onChange={(e) => setIdentifier(e.target.value)} />
-            <input type="text" placeholder='Lösenord' value={password}
-            onChange={(e) => setPassword(e.target.value)} />
+            <input 
+            type="text" 
+            placeholder='E-mail eller användarnamn' 
+            value={identifier}
+            onChange={(e) => setIdentifier(e.target.value)} 
+            />
+
+            <input 
+            type="text" 
+            placeholder='Lösenord' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+            />
 
             <button type='submit'>Logga in</button>
         </form>
