51c67dd95d0de2cea24cb9222de821b0cac8db8d
Luna <lunahjort@hotmail.com>
2026-05-25 05:32:26 +0200

    hämtning av alla användare och deras uppgifter, förutom lösenord

--- marketplace-backend/src/api/admin-users/controllers/admin-users.js
+++ marketplace-backend/src/api/admin-users/controllers/admin-users.js
@@ -1,1 +1,20 @@
 'use strict';
+
+module.exports = {
+    // hämta alla användare
+    async getUsers(ctx) {
+        try {
+            const users = await strapi.entityService.findMany(
+                'plugin::users-permissions.user',
+                {
+                    fields: ['id', 'username', 'email', 'blcoked', 'createdAt'],
+                    populate: ['role'],
+                }
+            );
+            ctx.body = users;
+        } catch (err) {
+            ctx.throw(500, err);        
+        }
+    },
+
+}
