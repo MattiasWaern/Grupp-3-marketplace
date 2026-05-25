49239c4ec636c9a2a4ff6e90cf1db0784f3064f7
Luna <lunahjort@hotmail.com>
2026-05-25 06:04:41 +0200

    funktion och useEffect för hämtning av alla användare när sidan laddas

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,1 +1,18 @@
 import { useState, useEffect } from "react";
+
+function AdminPage() {
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState("");
+
+    // hämtar alla användare vid start
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+}
+
+AdminPage.route = {
+    path: '/admin',
+    index: 10,
+};
+
+export default AdminPage;
