dfdd0709a8e0a79b3997e6ff301fc2516fea615f
Luna <lunahjort@hotmail.com>
2026-05-25 11:32:27 +0200

    fetchUsers skapat och felhantering

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -8,6 +8,29 @@ function AdminPage() {
     useEffect(() => {
         fetchUsers();
     }, []);
+
+    const fetchUsers = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await fetch("http://localhost:1337/api/admin-users", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            const data = await response.json();
+
+            if(!response.ok) {
+                setError("Kunde inte hämta användare.");
+                return;
+            }
+
+            setUsers(data);
+        } catch (err) {
+            setError("Något gick fel");
+            console.error(err);
+        }
+    };
 }
 
 AdminPage.route = {
