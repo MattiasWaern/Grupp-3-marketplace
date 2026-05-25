0c3157ea7e653936784c824a9b6d09d8f4cc773c
Helena Rhawi <helena.rhawi@live.com>
2026-05-26 00:31:15 +0200

    cleanup

--- src/pages/Chatpage.jsx
+++ src/pages/Chatpage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import '../css/chatpage.css'
+
 
 function ChatPage(){
     const location = useLocation();
