ad7bae8fba98319eba1dd0b10930543b59487a26
Helena Rhawi <helena.rhawi@live.com>
2026-05-26 23:35:19 +0200

    lagt till custom controller och routes för reviews per listing med filtrering, bytt namn på filen custom-reviews.js

--- marketplace-backend/src/api/review/controllers/review.js
+++ marketplace-backend/src/api/review/controllers/review.js
@@ -8,8 +8,22 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::review.review', ({ strapi }) => ({
   async customReviews(ctx) {
+
+      console.log("🔥 ROUTE HIT");
+
+  console.log("PARAMS:", ctx.params);
     try {
-      ctx.body = 'ok';
+      const { listingId } = ctx.params;
+
+      const reviews = await strapi.entityService.findMany(
+        "api::review.review", 
+        {
+          filters: {
+            listing: listingId,
+          },
+        }
+      );
+      ctx.body = reviews;
     } catch (err) {
       ctx.body = err;
     }
--- marketplace-backend/src/api/review/routes/custom-reviews.js
+++ marketplace-backend/src/api/review/routes/custom-reviews.js
@@ -1,12 +1,12 @@
 module.exports = {
   routes: [
     {
-      method: 'GET',
-      path: '/reviews/listings/:listingsId',
+      method: "GET",
+      path: "/reviews/listings/:listingId",
       handler: "review.customReviews",
       config: {
         auth: false,
-      }
-    }
-  ]
-}
+      },
+    },
+  ],
+};
